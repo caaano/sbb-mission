39ace947406ec4ee41b4272434976ff9383c4b33
caaano <wlsrl0515@naver.com>
2023-11-24 14:07:32 +0900

    2-5, findBySubjectLike

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,11 +17,8 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        if(oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
 }
-
